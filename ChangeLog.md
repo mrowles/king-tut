d650bb9cbf68438d93283b1179a412a7bb583730
Elliot <elliot.smith@gmail.com>
2018-03-30 15:54:05 +1100

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for king-tut
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,55 @@
+import System.IO
+import System.Directory
+import Data.List
+
+main = do
+    let fileName = "test-file.txt"
+    let currDir = withCurrentDirectory
+    let backupFile = (fileName ++ ".tut.backup")
+    let tempFile = (fileName ++ ".tut.tmp")
+
+    -- Create handles for both the fileName and the backup file
+    originalFileHandle <- openFile fileName ReadMode
+    backupHandle <- openFile backupFile WriteMode
+
+    -- Passes the contents of the file into contents
+    contents <- hGetContents originalFileHandle
+
+    -- Creates the backup file
+    hPutStr backupHandle $ contents
+    hClose backupHandle
+
+    -- Testing parsing
+    putStrLn ("next test" ++ parseFile "" "" contents)
+
+    -- Create the new file with the handler
+    tempFileHandle <- openFile tempFile WriteMode
+    hPutStr tempFileHandle $ unlines newTodoItems
+    hClose tempFileHandle
+
+    -- Change the original file to remove the statement
+    removeFile fileName
+    renameFile tempFile fileName
+
+    -- Run the tests
+
+    -- Revert the file back to it's original state and delete other files
+    removeFile fileName
+    renameFile backupFile fileName
+
+goThroughFile :: String -> IO ()
+goThroughFile originalString = do
+    putStrLn originalString
+
+parseAndTestFile :: String -> String
+parseAndTestFile contents = do
+    parseFile "" "" contents
+
+--End of file
+parseFile :: String -> String -> String -> String
+parseFile before current "" = before
+
+--Parse File
+parseFile before current after = do
+    parseFile (before ++ [(after !! 0)]) "" (tail after)
+
